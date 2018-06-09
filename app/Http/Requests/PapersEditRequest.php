22c7c684ae154de0ae51b7b6c28f82697f9ca336
Carlos Riquelme <carlos.riquelme@gmail.com>
2018-06-09 13:27:55 -0400

    Listo el many to many de authors y papers

---
+++ app/Http/Requests/PapersEditRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class PapersEditRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            //
+            'titulo' => 'required|max:400',
+            'photo_id' => 'required|image',
+            'codigo' => 'required|max:9',
+            'año' => 'required',
+            'title_id' => 'required',
+            'teacher_id' => 'required'
+        ];
+    }
+}
---
+++ database/migrations/2018_06_11_190807_author_paper.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AuthorPaper extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('author_paper', function (Blueprint $table) {
+            $table->unsignedInteger('author_id')->nullable();
+            $table->foreign('author_id')->references('id')->on('authors');
+            $table->unsignedInteger('paper_id')->nullable();
+            $table->foreign('paper_id')->references('id')->on('papers');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('author_paper');
+    }
+}
